0ba08f014f4e80334fed990dbcae338a8d932214
rayaraya <raya.orlova.98@mail.ru>
2017-04-25 20:29:07 +0300

    add functions for drawing data

--- public/script.js
+++ public/script.js
@@ -1,83 +1,47 @@
-//https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_day.geojson
-//модифицированный обработчик из библиотеки
-ol.events.condition.ctrlShiftKeysOnly = function(mapBrowserEvent) {
-    var originalEvent = mapBrowserEvent.originalEvent;
-       return (!originalEvent.altKey &&
+'use strict';
+    var ws;
+    //модифицированный обработчик из библиотеки
+    ol.events.condition.ctrlShiftKeysOnly = function(mapBrowserEvent) {
+        var originalEvent = mapBrowserEvent.originalEvent;
+        return (!originalEvent.altKey &&
         (ol.has.MAC ? originalEvent.metaKey : originalEvent.ctrlKey) &&
         originalEvent.shiftKey);
-};
-function wsConnect(coord){
-    var ws = new WebSocket("ws://localhost:7070");
-    ws.onopen = function(){
-        console.log("Opening a connection...");
-        try {
-            ws.send(coord);
-        } catch (error) {
-            if (ws.readyState !== 1) {
-                var waitSend = setInterval(ws.send(coord), 1000);
-            }
-        }
     };
-    ws.onmessage = function(event){
-        console.log("msg " + event.data)
-            info2Box.innerHTML = event.data;
-    };
-    ws.onclose = function (event) {
-        console.log("I'm sorry. Bye!");
-    };
-    but.onclick = function(event) {
-        ws.close();
-    }
-}
-function changePlace(){
-    car.setGeometry(new ol.geom.Point(ol.proj.transform([30.216170226499887,59.95263361646573], 'EPSG:4326', 'EPSG:3857')))
-}
-//
-var car = new ol.Feature({
-                geometry: new ol.geom.Point(ol.proj.transform([30.214218875847227,59.944862260488776], 'EPSG:4326', 'EPSG:3857'))
-            });
-var car_source = new ol.source.Vector({features: Array(car)});
-//dot_source.addFeatures();
-var car_style = new ol.style.Style({
-                image: new ol.style.Icon({
-                            src: "./car.ico"
-                        })
-});
-var car_layer = new ol.layer.Vector({
-            title: 'VectorLayer1',
-            source: car_source,
-            /*style: new ol.style.Style({
-                    image:  new ol.style.Circle({
-                                radius: 3,
-                                fill: new ol.style.Fill({color: 'red'})
-                            })
-            })*/
-            style: car_style
-});
-var map = new ol.Map({
-    target: 'map',
-    layers: [
-        new ol.layer.Tile({
-            source: new ol.source.OSM(),
-        }), 
-        car_layer
-    ],
-    view: new ol.View({
-        //projection: 'EPSG:4326',
-        center: ol.proj.fromLonLat([30.2425,59.9426]),
-        zoom: 13
-        //maxResolution: 0.95
-    })
-});
-// 
-var infoBox = document.getElementById('info');
-var info2Box = document.getElementById('info2');
-var but = document.getElementById('sendButton');
-var dragBox = new ol.interaction.DragBox({
-    condition: ol.events.condition.platformModifierKeyOnly
-});
-map.addInteraction(dragBox);
-dragBox.on('boxend', function(){
+
+    var map = new ol.Map({
+        target: 'map',
+        layers: [
+            new ol.layer.Tile({
+                source: new ol.source.OSM()
+            })
+        ],
+        view: new ol.View({
+            //projection: 'EPSG:4326',
+            center: ol.proj.fromLonLat([30.2425,59.9426]),
+            zoom: 13
+            //  maxResolution: 0.703125
+        })
+    });
+
+    var source = new ol.source.Vector();
+
+    var vector = new ol.layer.Vector({
+          source: source
+        });
+
+    map.addLayer(vector);
+
+
+
+    var infoBox = document.getElementById('info');
+    var info2Box = document.getElementById('info2');
+    var but = document.getElementById('sendButton');
+    var dragBox = new ol.interaction.DragBox({
+        condition: ol.events.condition.platformModifierKeyOnly
+    });
+
+    map.addInteraction(dragBox);
+    dragBox.on('boxend', function(){       //разбить на функции
         infoBox.innerHTML = '&nbsp;';
         info2Box.innerHTML = '&nbsp;';
         var rectangle = dragBox.getGeometry().getCoordinates(false)[0];
@@ -113,8 +77,83 @@ dragBox.on('boxend', function(){
             + lowerLeft[1] + ','
             + upperRight[0] + ','
             + upperRight[1] + ']';
-        //console.log(str);
         infoBox.innerHTML = str;
-        var coord = {"d": str}
-        wsConnect(str);
-})
+
+        var strmin = + lowerLeft[0] + ','
+            + lowerLeft[1] + ','
+            + upperRight[0] + ','
+            + upperRight[1] ;
+        wsConnect(strmin);
+    })
+
+    var imageStyle = new ol.style.Style({
+        image: new ol.style.Circle({
+          radius: 3,
+          snapToPixel: false,
+          fill: new ol.style.Fill({color: 'blue'}),
+          stroke: new ol.style.Stroke({color: 'white', width: 1})
+        })
+      });
+    var coordinates = [];
+
+    function addData(cars){
+        prepareCrd(cars);
+        source.refresh();
+        //source.addFeatures(cars);           //обработчик change, можно использовать refresh()
+    }
+
+    function prepareCrd(cars){
+        //console.log(cars);
+        coordinates = [];
+        for(var i = 0; i < cars.length; i++){
+            coordinates.push(ol.proj.transform(cars[i], 'EPSG:4326', 'EPSG:3857'));
+        }
+        //console.log(coordinates);
+    }
+
+    function draw(event){
+
+        var listenerKey;
+
+        function animate(event){
+            var vectorContext = event.vectorContext;
+            vectorContext.setStyle(imageStyle);
+            vectorContext.drawGeometry(new ol.geom.MultiPoint(coordinates));
+            map.render();
+        }
+        listenerKey = map.on('postcompose', animate);
+    }
+
+    source.on('change', function(event){
+        draw(event);
+    })
+
+
+    function wsConnect(coord){
+        //ws = new WebSocket("ws://serene-plains-38004.herokuapp.com/");
+        var ws = new WebSocket("ws://localhost:7070/");
+        ws.onopen = function(){
+            console.log("Opening a connection...");
+            try {
+                    ws.send(coord);
+            } catch (error) {
+                    if (ws.readyState !== 1) {
+                        var waitSend = setInterval(ws.send(coord), 1000);
+                    }
+            }
+                };
+        ws.onmessage = function(event){
+            //console.log("msg " + event.data)
+            //info2Box.innerHTML = event.data;
+                //console.log(event.data);
+                //console.log(JSON.parse(event.data));
+                addData(JSON.parse(event.data));
+            };
+        ws.onclose = function (event) {
+                console.log("I'm sorry. Bye!");
+            };
+        but.onclick = function(event) {
+            ws.close();
+        }
+    }
+  
