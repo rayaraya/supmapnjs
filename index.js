b918d6b43ffbb6f44ff40ea961a6343e0e8ef762
rayaraya <raya.orlova.98@mail.ru>
2017-05-16 11:32:50 +0300

    add about page+instruction

--- index.js
+++ index.js
@@ -8,11 +8,13 @@ var app = express();
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function (req, res) {
-  res.sendFile('/index.html');
+  //res.sendFile('/index.html');
+  res.sendFile('/index.html', { root: __dirname + '/public' });
 });
 
 app.get('/about', function (req, res) {
-  res.sendFile('/about.html');
+  //res.sendFile('/about.html');
+  res.sendFile('/about.html', { root: __dirname + '/public' });
 });
 
 var port = process.env.PORT || 8080;
