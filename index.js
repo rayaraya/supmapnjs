3a106f9cf9873bf66a17cc3aac5e8f12f1b64447
rayaraya <raya.orlova.98@mail.ru>
2017-05-23 00:39:56 +0300

    limit zoom for rectangle + add tips and about page

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 var express = require('express');
-//var client = require('./public/client.js');
 
 var path = require('path');
 var app = express();
@@ -8,12 +7,10 @@ var app = express();
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function (req, res) {
-  //res.sendFile('/index.html');
   res.sendFile('/index.html', { root: __dirname + '/public' });
 });
 
 app.get('/about', function (req, res) {
-  //res.sendFile('/about.html');
   res.sendFile('/about.html', { root: __dirname + '/public' });
 });
 
--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,49 +1,6 @@
 'use strict';
 
-function setHeight() {
-    $('#map').css({
-        height: ($(window).height() - 50) + 'px'
-    });
-}
-
 setHeight();
-$(window).resize(setHeight); 
-
-var speed = $('#speed').slider({
-    formatter: function(value) {
-        return 'X: ' + value;
-    }
-});
-
-speed.on('change', function(event){
-    if(ws != undefined && !(ws.readyState === ws.CLOSED)){
-        ws.send(JSON.stringify({"speed_change": event.value}));
-        console.log("speed_change");
-    }
-});
-
-var capacity = $('#capacity').slider({
-    formatter: function(value) {
-        return 'C: ' + value;
-    }
-});
-
-capacity.on('change', function(event){
-    if(ws != undefined && !(ws.readyState === ws.CLOSED)){
-        ws.send(JSON.stringify({"capacity_change": event.value}));
-        console.log("capacity_change");
-    }
-});
-
-
-// var r = $('#color').slider()
-//         .on('slide', colorChange)
-//         .data('slider');
-
-// var colorChange = function() {
-//     //$('#').css('background', 'rgb('+r.getValue()+','+g.getValue()+','+b.getValue()+')')
-//     cars_color = 'rgb('+r.getValue()+', 85, 110)';
-// };
 
 var ws;
 var pause_flag;
@@ -63,7 +20,7 @@ var map = new ol.Map({
     ],
     view: new ol.View({
         //projection: 'EPSG:4326',
-        center: ol.proj.fromLonLat([24.7306,59.4251]),
+        center: ol.proj.fromLonLat([24.6806,59.4151]),
         zoom: 14
         //  maxResolution: 0.703125
     })
@@ -74,6 +31,7 @@ var rect_source = new ol.source.Vector();
 var vector = new ol.layer.Vector({
       source: source
     });
+
 var rect_vector = new ol.layer.Vector({
       source: rect_source,
       style: new ol.style.Style({
@@ -87,6 +45,14 @@ var rect_vector = new ol.layer.Vector({
         })
     });
 
+var imageStyle = new ol.style.Style({
+    image: new ol.style.Circle({
+      radius: 3,
+      snapToPixel: false,
+      fill: new ol.style.Fill({color: 'blue'}),
+      stroke: new ol.style.Stroke({color: 'white', width: 1})
+    })
+  });
 
 map.addLayer(vector);
 map.addLayer(rect_vector);
@@ -100,71 +66,68 @@ ol.events.condition.ctrlShiftKeysOnly = function(mapBrowserEvent) {
 };
 
 dragBox.on('boxend', function(){
-    var rectangle = dragBox.getGeometry().getCoordinates(false)[0];
-    var vertexA = rectangle[0];
-    var vertexB = rectangle[1];
-    var vertexC = rectangle[2];
-    var vertexD = rectangle[3];
-    var lowerLeft, upperRight;
-    if(vertexA[0] != vertexB[0]) {
-        if (vertexA[0] < vertexB[0]) {
-            upperRight = vertexB;
-            lowerLeft = vertexD;
+    console.log(map.getView().getZoom());
+
+    if(map.getView().getZoom() >= 14){
+        var rectangle = dragBox.getGeometry().getCoordinates(false)[0];
+        var vertexA = rectangle[0];
+        var vertexB = rectangle[1];
+        var vertexC = rectangle[2];
+        var vertexD = rectangle[3];
+        var lowerLeft, upperRight;
+        if(vertexA[0] != vertexB[0]) {
+            if (vertexA[0] < vertexB[0]) {
+                upperRight = vertexB;
+                lowerLeft = vertexD;
+            }
+            else {
+                lowerLeft = vertexB;
+                upperRight = vertexD;
+            }
         }
         else {
-            lowerLeft = vertexB;
-            upperRight = vertexD;
+            if (vertexA[1] < vertexB[1]) {
+                lowerLeft = vertexA;
+                upperRight = vertexC;
+            }
+            else {
+                upperRight = vertexA;
+                lowerLeft = vertexC;
+            }
         }
+        lowerLeft = ol.proj.toLonLat(lowerLeft);
+        upperRight =  ol.proj.toLonLat(upperRight);
+        var str = 'http://www.overpass-api.de/api/xapi?way[bbox='
+            + lowerLeft[0] + ','
+            + lowerLeft[1] + ','
+            + upperRight[0] + ','
+            + upperRight[1] + ']';
+
+        var strmin = + lowerLeft[0] + ','
+            + lowerLeft[1] + ','
+            + upperRight[0] + ','
+            + upperRight[1] ;
+
+        lowerLeft = ol.proj.transform(lowerLeft, 'EPSG:4326', 'EPSG:3857');
+        upperRight = ol.proj.transform(upperRight, 'EPSG:4326', 'EPSG:3857');
+
+        var polyCoords = [];
+        polyCoords.push(lowerLeft);
+        polyCoords.push([lowerLeft[0],upperRight[1]]);
+        polyCoords.push(upperRight);
+        polyCoords.push([upperRight[0],lowerLeft[1]]);
+        polyCoords.push(lowerLeft);
+
+        var feature = new ol.Feature({
+            geometry: new ol.geom.Polygon([polyCoords])
+        });
+        rect_source.addFeature(feature);
+
+        wsConnect(strmin); 
+    }else{
+
     }
-    else {
-        if (vertexA[1] < vertexB[1]) {
-            lowerLeft = vertexA;
-            upperRight = vertexC;
-        }
-        else {
-            upperRight = vertexA;
-            lowerLeft = vertexC;
-        }
-    }
-    lowerLeft = ol.proj.toLonLat(lowerLeft);
-    upperRight =  ol.proj.toLonLat(upperRight);
-    var str = 'http://www.overpass-api.de/api/xapi?way[bbox='
-        + lowerLeft[0] + ','
-        + lowerLeft[1] + ','
-        + upperRight[0] + ','
-        + upperRight[1] + ']';
-
-    var strmin = + lowerLeft[0] + ','
-        + lowerLeft[1] + ','
-        + upperRight[0] + ','
-        + upperRight[1] ;
-
-    lowerLeft = ol.proj.transform(lowerLeft, 'EPSG:4326', 'EPSG:3857');
-    upperRight = ol.proj.transform(upperRight, 'EPSG:4326', 'EPSG:3857');
-
-    var polyCoords = [];
-    polyCoords.push(lowerLeft);
-    polyCoords.push([lowerLeft[0],upperRight[1]]);
-    polyCoords.push(upperRight);
-    polyCoords.push([upperRight[0],lowerLeft[1]]);
-    polyCoords.push(lowerLeft);
-
-    var feature = new ol.Feature({
-        geometry: new ol.geom.Polygon([polyCoords])
-    });
-    rect_source.addFeature(feature);
-
-    wsConnect(strmin);
 })
-
-var imageStyle = new ol.style.Style({
-    image: new ol.style.Circle({
-      radius: 3,
-      snapToPixel: false,
-      fill: new ol.style.Fill({color: 'blue'}),
-      stroke: new ol.style.Stroke({color: 'white', width: 1})
-    })
-  });
 
 function addData(cars){
     prepareCrd(cars);
@@ -193,15 +156,6 @@ function animate(event){
         map.render();
 }
 
-rect_source.on('clear', function(){
-    coordinates = [];
-    source.refresh();
-})
-
-source.on('change', function(){
-    draw();
-})
-
 function wsConnect(coord){
     //ws = new WebSocket("wss://serene-plains-38004.herokuapp.com/");
     ws = new WebSocket("ws://localhost:7070/");
@@ -228,21 +182,49 @@ function wsConnect(coord){
     };
 }
 
-$('#play').on('click', function(){
-    // if(ws != undefined && !(ws.readyState === ws.CLOSED) && pause_flag === 1){
-    //     ws.send(JSON.stringify({"button_msg":"play"}));
-    //     console.log("play");
-    //     pause_flag = 0;
-    // }
+rect_source.on('clear', function(){
+    coordinates = [];
+    source.refresh();
 })
 
-$('#pause').on('click', function(){
-    // if(ws != undefined && !(ws.readyState === ws.CLOSED && pause_flag === 0)){
-    //     ws.send(JSON.stringify({"button_msg":"pause"}));
-    //     console.log("pause");
-    //     pause_flag = 1;
-    // }
+source.on('change', function(){
+    draw();
 })
+
+
+function setHeight() {
+    $('#map').css({
+        height: ($(window).height() - 50) + 'px'
+    });
+}
+
+var speed = $('#speed').slider({
+    formatter: function(value) {
+        return 'X: ' + value;
+    }
+});
+
+speed.on('change', function(event){
+    if(ws != undefined && !(ws.readyState === ws.CLOSED)){
+        ws.send(JSON.stringify({"speed_change": event.value}));
+        console.log("speed_change");
+    }
+});
+
+var capacity = $('#capacity').slider({
+    formatter: function(value) {
+        return 'C: ' + value;
+    }
+});
+
+capacity.on('change', function(event){
+    if(ws != undefined && !(ws.readyState === ws.CLOSED)){
+        ws.send(JSON.stringify({"capacity_change": event.value}));
+        console.log("capacity_change");
+    }
+});
+
+$(window).resize(setHeight); 
 
 $('#close').on('click', function(){
     if(ws != undefined && !(ws.readyState === ws.CLOSED)){
